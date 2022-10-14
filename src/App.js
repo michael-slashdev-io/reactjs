cf16769c10c200675e0beef1c92f584e0fda4e0b
Michael Ballard <michaelballard12@gmail.com>
2022-10-14 16:03:00 +0200

    latest

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './components/Card';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div>
+      <h1>slashdev.io</h1>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import styles from './styles/Card.css';
+
+function Card() {
+    return (
+        <div className='Card'>
+        <h2>Excuses 2</h2>
+        <div className='actions'>
+          <button className='btn'>Delete</button>
+        </div>
+      </div>  
+    );
+}
+
+export default Card;
